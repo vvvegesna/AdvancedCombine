868412d9b382753f0d8894d187a63771c7411bbe
Vegesna, Vijay Varma <vijayv.vegesna@iqvia.com>
2022-07-09 15:13:14 -0700

    Initial Commit

---
+++ Shared/AdvancedCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCombineApp.swift
+//  Shared
+//
+//  Created by Vegesna, Vijay Varma on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
